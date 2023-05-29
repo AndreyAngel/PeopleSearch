1a69a9a842ea7768fae22981688bf74db388e00c
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2023-05-29 16:41:32 +0700

    Debugging (Mapping)

--- src/PeopleSearchAPI/Helpers/MappingProfile.cs
+++ src/PeopleSearchAPI/Helpers/MappingProfile.cs
@@ -34,6 +34,8 @@ public class MappingProfile : Profile
 
         CreateMap<UserQuestionnaireModel, UserQuestionnaireDTOResponse>();
 
+        CreateMap<UserQuestionnaireModel, UserQuestionnaireListDTOResponse>();
+
         CreateMap<GradeDTORequest, GradeModel>();
 
         CreateMap<InterestDTO, InterestModel>();
